a3be08815653e429251c2dabeb91b645a0f3143a
Modjas726 <argamodzasentosa106@gmail.com>
2026-07-17 01:22:52 +0700

    hapuss celeng

--- Bantuan.js
+++
@@ -1,7 +0,0 @@
-// Logika interaktivitas akordeon FAQ
-function toggleFaq(headerElement) {
-    const parentRow = headerElement.parentElement;
-    
-    // Toggle class active pada baris FAQ yang diklik
-    parentRow.classList.toggle('active');
-}
--- bantuan.js
+++
@@ -1,5 +0,0 @@
-function toggleFaq(headerElement) {
-    const parentRow = headerElement.parentElement;
-
-    parentRow.classList.toggle('active');
-}
