c76bbfe5873c5db01c5d88320f6f42af5e30cb5f
Modjas726 <argamodzasentosa106@gmail.com>
2026-07-17 01:19:58 +0700

    c

--- bantuan.js
+++ bantuan.js
@@ -1,5 +1,7 @@
+// Logika interaktivitas akordeon FAQ
 function toggleFaq(headerElement) {
     const parentRow = headerElement.parentElement;
-
+    
+    // Toggle class active pada baris FAQ yang diklik
     parentRow.classList.toggle('active');
 }
