d92401ed6ddc0e64ca441a36aedc6cdab5580f68
Kazuto.Ohshiba <farizirawan789@gmail.com>
2026-07-17 05:10:16 +0700

    top global figmma

--- Pengaturan.js
+++ Pengaturan.js
@@ -1,18 +1,18 @@
-// Validasi & Trigger Ubah Kata Sandi Akun
+
 function changePassword(event) {
     event.preventDefault();
     
     const newPass = document.getElementById('st-newpass').value;
     const confirmPass = document.getElementById('st-confirmpass').value;
     
-    // Validasi apakah password baru dan konfirmasi cocok
+    
     if (newPass !== confirmPass) {
         alert("Konfirmasi kata sandi baru tidak cocok! Periksa kembali.");
         document.getElementById('st-confirmpass').focus();
         return;
     }
     
-    // Memunculkan popup modal sukses
+    
     document.getElementById('settingsSuccessModal').style.display = 'flex';
     document.getElementById('passwordForm').reset();
 }
